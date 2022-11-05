105351ef2a97ef0b8e6f7f02738612a12b5a7889
efeguerrero <juanfg.8640@gmail.com>
2022-11-05 15:05:56 -0300

    toggle menu ok

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import CartWidget from './CartWidget';
 import menuIcon from '../imgs/icons/menuicon.svg';
 import mainLogo from '../imgs/logos/logostefanos.png';
 
 const NavBar = () => {
+  const refFilterContainer = useRef(null);
+  const refMenuIcon = useRef(null);
+
+  const handleMenuClick = () => {
+    refFilterContainer.current.classList.toggle('navbarFilterContainer-active');
+    refMenuIcon.current.classList.toggle('navbarMenuIcon-active');
+  };
+
   return (
     <nav className="navbar">
-      <img src={menuIcon} alt="Menu Icon" className="navbarMenuIcon" />
+      <img
+        src={menuIcon}
+        onClick={handleMenuClick}
+        alt="Menu Icon"
+        className="navbarMenuIcon"
+        ref={refMenuIcon}
+      />
       <img src={mainLogo} alt="" className="navbarLogo" />
-      <ul className="navbarFilterContainer">
+      <ul className="navbarFilterContainer" ref={refFilterContainer}>
         <li className="navbarFilterItem">
           <button className="navbarFilterBtn">Todo</button>
         </li>
