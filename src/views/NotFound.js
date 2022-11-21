fb95e2425f2ba1a73d41279361c0f98436e622bf
efeguerrero <juanfg.8640@gmail.com>
2022-11-21 14:18:28 -0300

    Error Page Done

--- src/views/NotFound.js
+++ src/views/NotFound.js
@@ -1,14 +1,25 @@
 import { useRouteError } from 'react-router-dom';
 
+//Logo Import
+import mainLogo from '../assets/logos/logostefanos.png';
+
+//React-Router-Dom Import (Logo link back home)
+import { Link } from 'react-router-dom';
+
 const NotFound = () => {
   const error = useRouteError();
   console.log(error);
 
   return (
-    <div>
-      <h1>404</h1>
-      <p>Pagina no encontrada</p>
-      <button>Volver al Home</button>
+    <div className="errorContainer">
+      <Link to="/">
+        <img className="errorLogo" src={mainLogo} alt="Stefanos Loco" />
+      </Link>
+      <h1 className="errorCode">Error 404 :( </h1>
+      <p className="errorText">La página que buscas no existe</p>
+      <Link to="/">
+        <button className="errorBackBtn">Volver al Home</button>
+      </Link>
     </div>
   );
 };
