43fb47a3dd57b17057dd2360936ce2bb13d44070
efeguerrero <juanfg.8640@gmail.com>
2022-12-17 19:09:12 -0300

    order posting and stock update in firestore done

--- src/components/CustomerCheckout.js
+++ src/components/CustomerCheckout.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 
 //Context Imports
 import { useCartContext } from '../context/CartContex';
@@ -8,11 +8,15 @@ import {
   addDoc,
   collection,
   doc,
+  getDoc,
   getFirestore,
   updateDoc,
 } from 'firebase/firestore';
 
 const CustomerCheckout = () => {
+  const [orderId, setOrderId] = useState(null);
+  const [orderPosted, setOrderPosted] = useState(false);
+
   //Cart Context imports
   const { cart, cartTotal, clearCart } = useCartContext();
 
@@ -22,6 +26,8 @@ const CustomerCheckout = () => {
   const refEmail = useRef();
   const refPhone = useRef();
   const refAddress = useRef();
+
+  //Function to handle order final submit
 
   const handleSubmitCheckout = (e) => {
     e.preventDefault();
@@ -55,14 +61,53 @@ const CustomerCheckout = () => {
     const db = getFirestore();
     const ordersCollection = collection(db, 'orders');
 
+    //If promise is succesfull we store our ID to later show to user, and set our OrderPosted status to true, to re render and show our final message to customer. We also clear cart
+
     addDoc(ordersCollection, order)
-      .then(({ id }) => console.log(id))
+      .then(({ id }) => {
+        setOrderId(id);
+        clearCart();
+        setOrderPosted(true);
+      })
       .catch((err) => console.error({ err }));
   };
+
+  //Only update firebase db stock for products AFTER our ourder has been posted to avoid false stock. We use useEffect triggered by orderPosted state to ensure that we only run this after our order posting was succesfull
+
+  useEffect(() => {
+    if (orderPosted) {
+      const db = getFirestore();
+
+      // for each item in cart we get our current stock from firebase and then update that stock substracting the quantity in our cart for that id
+      cart.forEach((item) => {
+        //create reference for each item in cart to use in getDoc and updateDoc
+        const itemRef = doc(db, 'items', item.id);
+        getDoc(itemRef)
+          .then((result) => result.data().stock)
+          .then((originalStock) =>
+            updateDoc(itemRef, { stock: originalStock - item.quantity })
+          )
+          .catch((err) => console.error(err));
+      });
+    }
+  }, [orderPosted]);
+
+  if (orderPosted) {
+    return (
+      <section className="sectionCenter">
+        <h2 className="checkoutOrderTitle">¡Gracias por su compra!</h2>
+        <h3 className="checkoutOrderId">Numero de Orden: {orderId}</h3>
+      </section>
+    );
+  }
 
   return (
     <section className="sectionCenter">
       <form action="" className="checkoutForm" onSubmit={handleSubmitCheckout}>
+        <h2 className="checkoutFormAmount">
+          El monto total de su compra es de: $
+          {cartTotal().toLocaleString('es-AR')}
+        </h2>
         <input
           type="text"
           name="name"
